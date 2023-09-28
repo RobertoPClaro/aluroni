4444c3c0921da676214c3d58650868a775f51ff4
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-09-28 17:14:44 -0300

    Criando Filtros e Ordenador

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,10 +2,14 @@ import styles from './Cardapio.module.scss'
 import { ReactComponent as Logo } from 'assets/logo.svg'
 import Buscador from './Buscador'
 import { useState } from 'react'
+import Filtros from './Filtros';
+import Ordenador from './Ordenador';
 
 export default function Cardapio() {
 
   const [busca, setBusca] = useState("");
+  const [filtro, setFiltro] = useState<number | null>(null);
+  const [ordenador, setOrdenador] = useState("");
 
   return (
     <main>
@@ -23,6 +27,16 @@ export default function Cardapio() {
           busca={busca}
           setBusca={setBusca}
         />
+        <div className={styles.cardapio__filtros}>
+          <Filtros
+            filtro={filtro}
+            setFiltro={setFiltro}
+          />
+          <Ordenador
+            ordenador={ordenador}
+            setOrdenador={setOrdenador}
+          />
+        </div>
       </section>
     </main>
   )
