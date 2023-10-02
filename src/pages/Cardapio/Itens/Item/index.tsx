b9a802f60bef23dbfb40cabdc00002f418414d06
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2023-10-02 17:22:23 -0300

    Adicionando o eslint e criando Pagina inicio

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import styles from './Item.module.scss'
-import classNames from 'classnames'
+import React from 'react';
+import styles from './Item.module.scss';
+import classNames from 'classnames';
 
 
 interface Props{
@@ -19,32 +19,32 @@ interface Props{
 export default function Item({title, description, category, size, serving, price, photo}: Props) {
   return (
     <div className={styles.item}>
-        <div className={styles.item__imagem}>
-            <img src={photo} alt={title} />
+      <div className={styles.item__imagem}>
+        <img src={photo} alt={title} />
+      </div>
+      <div className={styles.itenm__descricao}>
+        <div className={styles.item__titulo}>
+          <h2> {title}</h2>
+          <p> {description}</p>
         </div>
-        <div className={styles.itenm__descricao}>
-            <div className={styles.item__titulo}>
-                <h2> {title}</h2>
-                <p> {description}</p>
-            </div>
-            <div className={styles.item__tags}>
-                <div className={classNames({
-                    [styles.item__tipo]:true,
-                    [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,               
-                })}>
-                    {category.label}
-                </div>
-                <div className={styles.item__porcao}>
-                    {size}g
-                </div>
-                <div className={styles.item__qtdpessoas}>
-                    Serve {serving} pessoa{serving === 1 ? "": "s"}
-                </div>
-                <div className={styles.item__valor}>
+        <div className={styles.item__tags}>
+          <div className={classNames({
+            [styles.item__tipo]:true,
+            [styles[`item__tipo__${category.label.toLowerCase()}`]]: true,               
+          })}>
+            {category.label}
+          </div>
+          <div className={styles.item__porcao}>
+            {size}g
+          </div>
+          <div className={styles.item__qtdpessoas}>
+                    Serve {serving} pessoa{serving === 1 ? '': 's'}
+          </div>
+          <div className={styles.item__valor}>
                     R$ {price.toFixed(2)}
-                </div>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,16 +1,16 @@
-import styles from './Cardapio.module.scss'
-import { ReactComponent as Logo } from 'assets/logo.svg'
-import Buscador from './Buscador'
-import { useState } from 'react'
+import styles from './Cardapio.module.scss';
+import { ReactComponent as Logo } from 'assets/logo.svg';
+import Buscador from './Buscador';
+import { useState } from 'react';
 import Filtros from './Filtros';
 import Ordenador from './Ordenador';
 import Itens from './Itens';
 
 export default function Cardapio() {
 
-  const [busca, setBusca] = useState("");
+  const [busca, setBusca] = useState('');
   const [filtro, setFiltro] = useState<number | null>(null);
-  const [ordenador, setOrdenador] = useState("");
+  const [ordenador, setOrdenador] = useState('');
 
   return (
     <main>
@@ -41,5 +41,5 @@ export default function Cardapio() {
         <Itens busca={busca} filtro={filtro} ordenador={ordenador}/>
       </section>
     </main>
-  )
+  );
 }
